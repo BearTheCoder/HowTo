c57c828aec08d67676b4bd8fc56986327bff9a3a
Bear <58434823+BearTheCoder@users.noreply.github.com>
2022-10-27 21:27:33 -0400

    add file

---
+++ JavaScript/SQL_Railway_to_Hostgator.js
@@ -0,0 +1,20 @@
+//Connects to my database on HostGator from an app running on Railway
+
+function callSQL(){
+  var mysql = require('mysql');
+  var connection = mysql.createConnection({
+      host: 'www.bearthecoder.tech', // HostGator website
+      user: 'username',
+      password: 'password',
+  });
+
+  connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+  });
+}
+
+module.exports = {callSQL};
