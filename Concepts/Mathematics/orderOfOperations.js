e70a225c37c29281dccad7241a403c1ddf17a41d
BearTheCoder <bearthecoder@gmail.com>
2022-12-08 16:21:57 -0500

    oRDER OF OP

---
+++ Concepts/Mathematics/orderOfOperations.js
@@ -0,0 +1,6 @@
+// As you may be aware, the order of operations is something that you must be aware of whenever you do your calculations.
+// Some people are taught PEMDAS but most languages use PEDMAS
+
+let result = 6 / 2 * (1 + 2); // 9? or 1?
+
+console.log(result); // Outputs 9
