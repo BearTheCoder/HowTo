2b830f22fce69c2200af368e35931f9d9768741b
Bear <bearthecoder@gmail.com>
2023-01-20 18:50:22 -0500

    Update getUserLoginFromToken.cs

--- Code/C-Sharp/Twitch/getUserLoginFromToken.cs
+++ Code/C-Sharp/Twitch/getUserLoginFromToken.cs
@@ -4,4 +4,23 @@ requestMethod.Headers.Add("Authorization", "Bearer <user access token>");
 requestMethod.Headers.Add("Client-Id", "<client-id>");
 HttpResponseMessage response = await client.SendAsync(requestMethod);
 string responseBody = await response.Content.ReadAsStringAsync();
-Console.WriteLine(responseBody.Split("\"login\":\"")[1].Split("\"")[0]);
+Console.WriteLine(responseBody.Split("\"login\":\"")[1].Split("\"")[0]); // "login" can be substituted for an param in response.
+
+
+// EXAMPLE RESPONSE:
+// {
+//     "data": [
+//         {
+//             "id": "177484095",
+//             "login": "bearthecoder",
+//             "display_name": "BearTheCoder",
+//             "type": "",
+//             "broadcaster_type": "",
+//             "description": "Eh. ",
+//             "profile_image_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/5f615ed5-fd5e-4599-be2c-71e4690bfd27-profile_image-300x300.png",
+//             "offline_image_url": "",
+//             "view_count": 28,
+//             "created_at": "2017-10-13T01:27:50Z"
+//         }
+//     ]
+// }
