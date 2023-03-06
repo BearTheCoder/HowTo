65306216983bea40950c679fc562af381af9323f
BearTheCoder <bearthecoder@gmail.com>
2023-03-05 19:47:54 -0500

    Coordinate Dependant Random Numbers

--- Code/Python/helloWorld.py
+++ Code/Python/helloWorld.py
@@ -1,9 +1,30 @@
 # To run this application, open a terminal at this file's location and type "py <insert file name>"
+# If there is something that you are wanting to know that is not here, it is the same as any other language. Google it.
 
+# print and input is the same as console.log() and console.writeline()
 print("Hello World!!!");
 print("Hello " + input("What is your name? "));
+
 # This is a comment.
 print("\"Hello World!\" is this many digits long:")
+
+# "len()" is the same as string.length
 print(len("Hello World!"))
+
+# Variables do not need a type nor a keyword.
 occupation = input("What do you do for a living? ");
 print(occupation + "? That's awesome!");
+
+# "type()" will get what primitive the variable is.
+print(type(occupation)) #outputs string
+
+# Type conversions are done with functions. str() == .ToString(), float() == (float), int() == Parse.ToInt().
+# (thought) it almost seems as though python does not have classes and objects, therefore no methods, which is why everything is a function.
+age = 30;
+ageString = str(age); # "30"
+print("I am " + ageString + " years old.")
+
+# Division output is a float
+print(type(6/3)) # outputs 2.0
+
+print(2 ** 3) # exponential operator "**"
---
+++ Concepts/Random Numbers/Coordinate Dependant Random Noise/script.js
@@ -0,0 +1,46 @@
+function setup () {
+  createCanvas(400, 400);
+  background(0);
+  // randomPixelValues(); //p5js random()
+  coordinateDependantRandom(); //
+}
+
+function randomPixelValues () {
+  for (let x = 0; x < 400; x++) {
+    for (let y = 0; y < 400; y++) {
+      //Generate Random Number between 0-255
+      const randNum = random(0, 255);
+      push();
+      noStroke();
+      fill(randNum);
+      square(x, y, 1);
+      pop();
+    }
+  }
+}
+
+function coordinateDependantRandom () {
+  for (let x = 0; x < 400; x++) {
+    for (let y = 0; y < 400; y++) {
+      //Generate Random Number between 0-255
+      const randNum = coordinateDependantLCG(x, y);
+      push();
+      noStroke();
+      fill(randNum);
+      square(x, y, 1);
+      pop();
+    }
+  }
+}
+
+function coordinateDependantLCG (x, y) {
+  x += 100;
+  y += 100;
+  const h = 7140275233 + x * 374761393 + y * 668265263;
+  const c = x * 374761393;
+  const seedXn = ((x * 12) + 7140275233) % y;
+  const m = 1012313;
+  const result = ((h * seedXn) + c) % m;
+  const normalizedNumber = result / m;
+  return normalizedNumber * 255;
+}
